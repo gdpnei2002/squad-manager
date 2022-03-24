c61ca6e7f7b8c92501626429cf2732cd23c962f4
neilson <joseneilson25@gmail.com>
2022-03-24 20:49:03 -0300

    first commit

---
+++ squad manager/Models/UserViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+		public string Username { get; set; }
+
+		public string Email { get; set; }
+
+}		public string Password { get; set; }
+
+}
